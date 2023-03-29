a191d0176540508f0091e6e8a98819853069c76b
muhammedrizwan <rizwanmuhammed1947@gmail.com>
2023-03-29 19:50:26 +0530

    solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,6 +2,19 @@ package com.driver;
 
 public class Main {
 
+
+  public static   class Product{
+        public int product(int x, int y) {
+            return  x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+
+        public double product(double x, double y) {
+            return x*y;
+        }
+
     public static void main(String[] args) {
         Product p = new Product();
         int result1 = p.product(2, 3);
@@ -16,16 +29,6 @@ public class Main {
 }
 
 
-class Product{
-    public int product(int x, int y) {
-        return  x*y;
-    }
-    public int product(int x, int y, int z) {
-        return x*y*z;
-    }
 
-    public double product(double x, double y) {
-        return x*y;
-    }
 }
 
